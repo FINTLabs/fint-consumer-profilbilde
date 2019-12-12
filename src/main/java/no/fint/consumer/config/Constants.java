defdab9d5193f57f4244cbc413357a5c897906b3
Asgeir Nilsen <asgeir@fintlabs.no>
2019-12-12 11:27:58 +0100

    Rename avatar to profilbilde everywhere

--- src/main/java/no/fint/consumer/config/Constants.java
+++ src/main/java/no/fint/consumer/config/Constants.java
@@ -3,7 +3,7 @@ package no.fint.consumer.config;
 public enum Constants {
     ;
 
-    public static final String COMPONENT = "avatar";
+    public static final String COMPONENT = "profilbilde";
     public static final String COMPONENT_CONSUMER = COMPONENT + " consumer";
     public static final String CACHE_SERVICE = "CACHE_SERVICE";
 }
--- src/main/java/no/fint/consumer/config/LinkMapper.java
+++ src/main/java/no/fint/consumer/config/LinkMapper.java
@@ -3,14 +3,14 @@ package no.fint.consumer.config;
 import com.google.common.collect.ImmutableMap;
 import no.fint.consumer.utils.RestEndpoints;
 import no.fint.model.resource.Link;
-import no.fint.model.resource.avatar.AvatarResource;
+import no.fint.model.resource.profilbilde.ProfilbildeResource;
 
 import java.util.Map;
 
 public class LinkMapper {
     public static Map<String, String> linkMapper(String contextPath) {
         return ImmutableMap.<String, String>builder()
-                .put(Link.getHrefPlaceholder(AvatarResource.class), contextPath + RestEndpoints.PROFILBILDE)
+                .put(Link.getHrefPlaceholder(ProfilbildeResource.class), contextPath + RestEndpoints.PROFILBILDE)
                 .put("administrasjon.personal.personalressurs", "/administrasjon/personal/personalressurs")
                 .put("utdanning.elev.elev", "/utdanning/elev/elev")
                 .build();
